0507747bd86cb5ac23e0006a9ee99de842122cce
alanframe <alan@frameonline.co.uk>
2017-07-16 22:12:38 +0100

    Update skininfo.js

--- skininfo.js
+++ skininfo.js
@@ -2,7 +2,7 @@ define([], function () {
     'use strict';
 
     return {
-        id: 'defaultskin',
-        name: 'Default Skin'
+        id: 'alancustomised',
+        name: 'Alans customised Default Skin'
     };
 });
